a09ce523ea8bc79707f79b880fdd061e3febe203
Alice Lazzeri <alice.lazzeri@hotmail.it>
2024-06-09 10:08:30 +0200

    fixed user CRUD and auth methods

--- src/main/java/it/epicode/capstone_project_alicelazzeri/services/UserService.java
+++ src/main/java/it/epicode/capstone_project_alicelazzeri/services/UserService.java
@@ -13,7 +13,6 @@ import jakarta.mail.internet.MimeMessage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.security.crypto.password.PasswordEncoder;
